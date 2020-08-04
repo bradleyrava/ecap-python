27b87bbc0d26a7f30d2c98ddc7fd40637f1e320b
Brad Rava <bradleyrava@gmail.com>
2020-08-04 16:20:37 -0700

    updated qp solver

--- Function Script.py
+++ Function Script.py
@@ -28,13 +28,13 @@ def eta_min_fcn(lambda_param, p_flip, pt, omega, basis_0, basis_1, basis_sum, ba
     Dmat = (2*((1/n) * basis_sum + lambda_param*omega))
     dvec_terms = pd.DataFrame([dvec_terms_fcn(p_flip[ii], basis_0[ii], basis_1[ii])
                                for ii in range(0, len(p_flip)-1)])
-    dvec = ((2/n)*dvec_terms.sum(axis=0)).to_numpy()
+    dvec = -1*((2/n)*dvec_terms.sum(axis=0)).to_numpy()
 
     ## Constraint vectors
     Amat = basis_0_grid[end_row]
     b_vec = np.array([0.0])
 
-    return_obj = qpsolvers.solve_qp(P=Dmat, q=dvec, A=Amat, b=b_vec)
+    return_obj = quadprog.solve_qp(Dmat, dvec, Amat.transpose(), b_vec, meq=1)[0]
     return return_obj
 
 
