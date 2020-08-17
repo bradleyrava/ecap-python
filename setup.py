7227ffc523ce576803491a013b6ee429130b7e32
Brad Rava <bradleyrava@gmail.com>
2020-08-16 23:09:50 -0700

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ecap',         
   packages = ['ecap'],   
-  version = '0.1.99',     
+  version = '0.1.991',
   license='GPLv3',       
   description = 'Implements the Excess Certainty Adjusted Probability adjustment procedure.',
   author = 'Bradley Rava',                  
   author_email = 'brava@marshall.usc.edu',     
   url = 'https://github.com/bradleyrava',   
-  download_url = 'https://github.com/bradleyrava/ecap-python/archive/0.1.99.tar.gz',
+  download_url = 'https://github.com/bradleyrava/ecap-python/archive/0.1.991.tar.gz',
   keywords = ['Excess Certainty', 'Emperical Bayes', 'probability', 'calibration', 'statistics'],  
   install_requires=[            
           'pandas',
@@ -17,6 +17,8 @@ setup(
           'quadprog',
           'statsmodels',
           'patsy',
+          'math',
+          'statistics
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
