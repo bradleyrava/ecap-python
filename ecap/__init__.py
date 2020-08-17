190ced9094874220aabf7852cc70034b7adee0bb
Brad Rava <bradleyrava@gmail.com>
2020-08-16 20:28:53 -0700

    updating available functions

--- ecap/__init__.py
+++ ecap/__init__.py
@@ -1,2 +1,3 @@
-from ecap.main import ecap
-from ecap.main import predict_ecap
+from ecap.main import ecap, predict_ecap
+from ecap.functions import greater_half_indicator, greater_half_indicator_vec, prob_flip_fcn, prob_flip_fcn_vec, dvec_terms_fcn, eta_min_fcn, risk_hat_fcn, risk_cvsplit_fcn, min_half_fcn, min_half_fcn_vec, mle_binomial, tweed_adj_fcn, tweedie_est
+from ecap.pasty_deriv import _eval_bspline_basis
