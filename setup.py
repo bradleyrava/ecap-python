969345c6fcb97df3b5bd97d842430866e9bb46c2
Brad Rava <bradleyrava@gmail.com>
2020-08-17 11:25:33 -0700

    ECAP Python Paclage

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'ecap',         
   packages = ['ecap'],   
-  version = '0.1',
+  version = '0.2',
   license='GPLv3',       
   description = 'ECAP Procedure in Python',
   long_description = 'Implements the Excess Certainty Adjusted Probability adjustment procedure as described in the paper "Irrational Exuberance: Correcting Bias in Probability Estimates" by Gareth James, Peter Radchenko, and Bradley Rava (Journal of the American Statistical Association, 2020; <doi:10.1080/01621459.2020.1787175>). The package includes a function that preforms the ECAP adjustment and a function that estimates the parameters needed for implementing ECAP.',
   author = 'Bradley Rava',                  
   author_email = 'brava@marshall.usc.edu',     
   url = 'https://github.com/bradleyrava',   
-  download_url = 'https://github.com/bradleyrava/ecap-python/archive/0.1.tar.gz',
+  download_url = 'https://github.com/bradleyrava/ecap-python/archive/ecap-0.2.tar.gz',
   keywords = ['Excess Certainty', 'Emperical Bayes', 'probability', 'calibration', 'statistics'],  
   install_requires=[            
           'pandas',
